5659b0f4453eb01ae72802192dc3896ca6d0fdb6
shikhar-batham <83720643+shikhar-batham@users.noreply.github.com>
2023-12-29 20:07:30 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import java.util.Date;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is Java Program");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
