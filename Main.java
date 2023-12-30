667f66962b9b117c9d75a67e3f2e5952ea6aabc5
shikhar-batham <83720643+shikhar-batham@users.noreply.github.com>
2023-12-30 15:31:07 +0530

    added student class

--- Main.java
+++ Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println(new Date());
         System.out.println("Hello");
         System.out.println("Added one more file.");
+
+        Student student=new Student();
+        student.sayHello();
     }
 }
